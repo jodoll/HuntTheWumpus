5616e5dc764eb44eb76e2519c59b8351cd44d0e9
Johannes Doll <git@johannesdoll.com>
2022-04-23 08:03:12 +0200

    Add koTest as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
 group = "com.johannesdoll.huntthewumpus"
 version = "1.0"
 
+val koTestVersion : String by project
+
 repositories {
     mavenCentral()
 }
@@ -15,6 +17,8 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$koTestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$koTestVersion")
 }
 
 tasks.test {
